8ba41cf5474d8d1ae74216359114ca2d4b90a55e
adriana.osmulska@highcom.pl <adriana.osmulska@highcom.pl>
2023-08-10 13:30:14 +0200

    tworzenie pdf

---
+++ tworzenie-pdf/tworzenie-pdf/Program.cs
@@ -0,0 +1,30 @@
+﻿using PuppeteerSharp;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+
+        var launchOptions = new LaunchOptions
+        {
+            Headless = true
+        };
+
+        using var browser = await Puppeteer.LaunchAsync(launchOptions);
+        using var page = await browser.NewPageAsync();
+
+        string htmlContent = "<html><body><h1>Hello, PDF!</h1></body></html>";
+        await page.SetContentAsync(htmlContent);
+
+        var pdfBytes = await page.PdfDataAsync();
+
+        // Save the PDF bytes to a file
+        string pdfFilePath = "output.pdf";
+        File.WriteAllBytes(pdfFilePath, pdfBytes);
+
+        Console.WriteLine("PDF file generated at: " + pdfFilePath);
+    }
+}
